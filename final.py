2b5b6611435df671fae47d6b1af9a51ea42d076f
Prot0ast <a_barragan1@u.pacific.edu>
2022-11-22 16:30:09 -0600

    Brute force is functional :)

--- final.py
+++ final.py
@@ -1,18 +1,51 @@
-# This is a sample Python script.
+# Author: Alan Barragan
+# Project Title: COMP 257 Final Project
+# Description: This is my implementation for Problem 5 Board Game problem which consists of 3 different algorithms:
+# Brute-Force, Greedy, and Dynamic Programming
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import random
+# Helper Functions
 
 
-def brute(num_colors, array_colors):
+def generate_random(n):
+    v = [0] * n
+    for i in range(n):
+        v[i] = i + 1
+    return v
+
+
+def subset_maker(bridges):
+    subsets = [[]]
+    for i in range(len(bridges) + 1):
+        for j in range(i):
+            subsets.append(bridges[j: i])
+    return subsets
+
+
+# Algorithm Implementations
+
+
+def brute(subsets):
     max_bridges = 0
+    for x in subsets:
+        n = len(x)
+        overlapping = False
+        for i in range(1, n):
+            if x[i-1] > x[i]:
+                overlapping = True
+        if not overlapping:
+            if len(x) > max_bridges:
+                max_bridges = len(x)
+    return max_bridges
 
 
 def main():
     # num_colors value to be changed whenever needed
     num_colors = 5
-    array_colors = []
+    # array_bridges = generate_random(num_colors)
+    print("Bridge set to be checked: ", [5, 4, 1, 2, 3])
+    subsets = subset_maker([5, 4, 1, 2, 3])
+    # print("Subsets for the bridge set: ", subsets)
+    print("Brute solution result: ", brute(subsets))
 
 
 # Press the green button in the gutter to run the script.
